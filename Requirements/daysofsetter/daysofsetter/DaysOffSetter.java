ff590fb1f896f0e048bbe56a90b520eee3390f6a
ckogioup <t8150052@aueb.gr>
2020-01-11 16:31:37 +0200

    Set Days off limits and preferences classes.

---
+++ Requirements/daysofsetter/daysofsetter/DaysOffSetter.java
@@ -0,0 +1,84 @@
+package daysofsetter;
+
+public class DaysOffSetter {
+	
+	private boolean DaysOff = false; 
+	private int maxNumDaysoff;
+	private boolean AbsenceSick = false; 
+	private int maxNumAbsenceSick;
+	private boolean WorkfromHome = false; 
+	private int maxNumWorkfromHome;
+	private boolean FieldWorkDays = false; 
+	private int maxNumFieldWorkDays;
+
+	public DaysOffSetter() {
+		super();
+	
+	}
+
+	public boolean isDaysOff() {
+		return DaysOff;
+	}
+
+	public void setDaysOff(boolean daysOff) {
+		DaysOff = daysOff;
+	}
+
+	public int getMaxNumDaysoff() {
+		return maxNumDaysoff;
+	}
+
+	public void setMaxNumDaysoff(int maxNumDaysoff) {
+		this.maxNumDaysoff = maxNumDaysoff;
+	}
+
+	public boolean isSickDaysOff() {
+		return AbsenceSick;
+	}
+
+	public void setSickDaysOff(boolean AbsenceSick) {
+		this.AbsenceSick = AbsenceSick;
+	}
+
+	public int getMaxNumSickDaysoff() {
+		return maxNumAbsenceSick;
+	}
+
+	public void setMaxNumSickDaysoff(int maxNumAbsenceSick) {
+		this.maxNumAbsenceSick = maxNumAbsenceSick;
+	}
+
+	public boolean isWorkfromHomeDaysOff() {
+		return WorkfromHome;
+	}
+
+	public void setWorkfromHomeDaysOff(boolean WorkfromHome) {
+		this.WorkfromHome = WorkfromHome;
+	}
+
+	public int getMaxNumWorkfromHomeDaysOff() {
+		return maxNumWorkfromHome;
+	}
+
+	public void setMaxNumWorkfromHomeDaysOff(int maxNumWorkfromHome) {
+		this.maxNumWorkfromHome = maxNumWorkfromHome;
+	}
+
+	public boolean isFieldWorkDays() {
+		return FieldWorkDays;
+	}
+
+	public void setFieldWorkDays(boolean fieldWorkDays) {
+		FieldWorkDays = fieldWorkDays;
+	}
+
+	public int getMaxNumFieldWorkDays() {
+		return maxNumFieldWorkDays;
+	}
+
+	public void setMaxNumFieldWorkDays(int maxNumFieldWorkDays) {
+		this.maxNumFieldWorkDays = maxNumFieldWorkDays;
+	}
+
+
+}
