324f3e88d0af35e7beecaea0cdf066ddc27bf17d
ckogioup <t8150052@aueb.gr>
2020-01-13 14:45:26 +0200

    CSV Class for requirements

---
+++ Requirements/daysofsetter/daysofsetter/DaysOffSetterSaveCSV.java
@@ -0,0 +1,61 @@
+package daysofsetter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+
+public class DaysOffSetterSaveCSV {
+	static String filename = "requierments.csv";
+	static Path pathToFile = Paths.get(filename);
+    public static void main(String[] args) throws IOException, CsvException {
+    	updateCSV("DaysOff", true, 3);
+    	readCSV();
+    	
+    }
+    
+    public static List<String[]> readCSV() throws IOException, CsvException {
+    	try (
+                Reader reader = Files.newBufferedReader(pathToFile.toAbsolutePath());
+                CSVReader csvReader = new CSVReader(reader);
+            ) {
+            	List<String[]> records = csvReader.readAll();
+            	for (String[] record : records) {
+            	    System.out.println("Name : " + record[0]);
+            	    System.out.println("Email : " + record[1]);
+            	    System.out.println("Phone : " + record[2]);
+            	    System.out.println("---------------------------");
+                }
+            	return records;
+            }
+            
+    } 
+    
+    public static void updateCSV(String update, boolean switcher, int numdays) throws IOException, CsvException {
+    	List<String[]> readed = readCSV();
+        try (
+        		CSVWriter writer = new CSVWriter(new FileWriter(filename, false));
+            ) {
+                for(String[] s: readed) {
+                	if(s[0].contentEquals(update)) {
+                		s[1] = String.valueOf(switcher);
+                		s[2] = String.valueOf(numdays);
+                		//readed.remove(s);
+                		break;
+                		
+                	}
+                }
+                
+                writer.writeAll(readed);
+        }
+    }
+}	
+
+
