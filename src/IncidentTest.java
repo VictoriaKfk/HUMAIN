ae647d55b280cbbacb3c833ba222fa76637b9d56
ckogioup <t8150052@aueb.gr>
2020-01-14 23:16:46 +0200

    junit

---
+++ src/IncidentTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+class IncidentTest {
+
+	@Test
+	void testMBaculum() {
+	}
+
+	@Test
+	void testMEntrapper() {	
+	}
+
+}
---
+++ src/hrManagement/ContainerTest.java
@@ -0,0 +1,79 @@
+package hrManagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContainerTest {
+
+	@Test
+	void testWireDependencies() {
+		
+	}
+
+	@Test
+	void testGetLogin() {
+		
+	}
+
+	@Test
+	void testObject() {
+		
+	}
+
+	@Test
+	void testGetClass() {
+		
+	}
+
+	@Test
+	void testHashCode() {
+		
+	}
+
+	@Test
+	void testEquals() {
+		
+	}
+
+	@Test
+	void testClone() {
+		
+	}
+
+	@Test
+	void testToString() {
+		
+	}
+
+	@Test
+	void testNotify() {
+		
+	}
+
+	@Test
+	void testNotifyAll() {
+		
+	}
+
+	@Test
+	void testWaitLong() {
+		
+	}
+
+	@Test
+	void testWaitLongInt() {
+		
+	}
+
+	@Test
+	void testWait() {
+		
+	}
+
+	@Test
+	void testFinalize() {
+		
+	}
+
+}
