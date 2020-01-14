fcddbf2983371688538bf1fdc9e85627008da0ed
TeoDelis <56597158+TeoDelis@users.noreply.github.com>
2020-01-14 19:33:20 +0200

    Update AttendanceTracker.java

--- AttendanceTracker.java
+++ AttendanceTracker.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 
 public class AttendanceTracker { 
 	
+	public int daysOfWeek;
+	public int weekOfMonth;
+	public int totalWeekHours;
+	public int monthsOfYear;
 	
 	private String ID; 
-	//maybe use a "department" field
 	private static ArrayList<Integer> weekHours = new ArrayList<Integer>(7); // ArrayList saving weekly working hours of an employee
 	private static ArrayList<Integer> seasonHours = new ArrayList<Integer>(12); // ArrayList saving monthly working hours of an employee for a season
 	private static ArrayList<Integer> seasonHoursLeft = new ArrayList<Integer>(12); // ArrayList saving monthly working hours an employee was absent, for a season (with delays also)
@@ -42,9 +45,41 @@ public class AttendanceTracker {
 		this.ID = ID;
 	}
 	
-	/*After log in, the user must select the "Just arrived" RadioBox and on his way out, he must select the "I'm leaving" RadioBox*/
+	public int getDaysOfWeek() {
+		return daysOfWeek;
+	}
 
-	public void weekTracker(int dailyHours) { // Parameter "dailyHours" will be taken from AttendanceCheckMenu GUI class (method hoursPerDay)
+	public void setDaysOfWeek(int daysOfWeek) {
+		this.daysOfWeek = daysOfWeek;
+	}
+	
+	public int getWeekOfMonth() {
+		return weekOfMonth;
+	}
+
+	public void setWeekOfMonth(int weekOfMonth) {
+		this.weekOfMonth = weekOfMonth;
+	}
+
+	public int getTotalWeekHours() {
+		return totalWeekHours;
+	}
+
+	public void setTotalWeekHours(int totalWeekHours) {
+		this.totalWeekHours = totalWeekHours;
+	}
+
+	public int getMonthsOfYear() {
+		return monthsOfYear;
+	}
+
+	public void setMonthsOfYear(int monthsOfYear) {
+		this.monthsOfYear = monthsOfYear;
+	}
+	
+	/**After log in, the user must select the "Just arrived" RadioBox and on his way out, he must select the "I'm leaving" RadioBox*/
+
+	public void weekTracker(int dailyHours) { // Parameter "dailyHours" will be taken from AttendanceApp class (method hoursPerDay)
 				weekHours.add(dailyHours);   
 			if (dailyHours == 0) {
 				weekFullDayAbsences.add(1);	
@@ -52,14 +87,14 @@ public class AttendanceTracker {
 		}
 	
 	public void seasonTracker(int totalWeekHours, int monthsOfYear) { 
-			for (int w = 1; w <= 4; w++) {
-				seasonHours.add(monthsOfYear, seasonHours.get(monthsOfYear) + totalWeekHours);
-				for (int d = 0; d <= 6; d++) {
-					seasonFullDayAbsences.add(monthsOfYear, seasonFullDayAbsences.get(monthsOfYear) + weekFullDayAbsences.get(d));
-				}
-				if (seasonHours.get(monthsOfYear) < 160) { // TODO 160 is  optional (I must use the exact hours per week an employee should normally work, according to his schedule)
-					seasonHoursLeft.add(monthsOfYear, 160 - seasonHours.get(monthsOfYear));
+				for (int w = 0; w <= 3; w++) {
+					seasonHours.add(monthsOfYear, (seasonHours.get(monthsOfYear) + totalWeekHours));
+					for (int d = 0; d <= 6; d++) {
+						seasonFullDayAbsences.add(monthsOfYear, seasonFullDayAbsences.get(monthsOfYear) + weekFullDayAbsences.get(d));
+					}
+					if (seasonHours.get(monthsOfYear) < 160) { // TODO 160 is  optional (I must use the exact hours per week an employee should normally work, according to his schedule)
+						seasonHoursLeft.add(monthsOfYear, 160 - seasonHours.get(monthsOfYear));
+					}
 				}
 			}
-		}
 	} // end AttendanceTracker
